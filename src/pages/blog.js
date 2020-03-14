f2822d0c241fb8f992583f420d7ce55a19c8562f
Codding BBQ <codingbbq@gmail.com>
2020-03-14 19:23:20 +0530

    Follow along to gatsby bootcamp on youtube

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPage = () => {
+    return(
+        <div>
+            <h1>Blog.</h1>
+            <h2>This is a blog page.</h2>
+        </div>
+    )
+}
+
+export default BlogPage;
