ef1c410dba3cf79d5aadb440d8308d06933d6408
Codding BBQ <codingbbq@gmail.com>
2020-03-14 19:41:43 +0530

    Use of Link function from gatsby to navigate to pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About.</h1>
             <h2>This is an about page.</h2>
+            <p>Contact me for more details <Link to='/contact'>Contact me</Link></p>
         </div>
     )
 }
